ba6ba21ac518d1794d4eee4f76cf812c405f29f1
Eugene Wang <fengbaoxp@gmail.com>
2013-12-12 15:05:37 +0800

    根据tour页码修改样例文件

--- tour/tour3.go
+++ tour/tour3.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
+	"net"
+	"os"
+	"time"
 )
 
 func main() {
-	fmt.Println("My favorite number is", rand.Intn(10))
+	fmt.Println("Welcome to the playground!")
+
+	fmt.Println("The time is", time.Now())
+
+	fmt.Println("And if you try to open a file:")
+	fmt.Println(os.Open("D:\\WorspaceEugene\\gopath\\src\\github.com\\fengbaoxp\\gosample\\tour\\tour2.go"))
+
+	fmt.Println("Or access the network:")
+	fmt.Println(net.Dial("tcp", "google.com"))
 }
